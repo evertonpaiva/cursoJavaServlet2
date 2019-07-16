89c393082dd1cf1b309927cc21cf93d659cbe5ae
Everton Paiva <evertonpaiva@gmail.com>
2019-07-16 11:26:40 -0300

    Renomeando arquivo

--- gerenciador/src/br/com/alura/gerenciador/servlet/NovaEmpresaServlet.java
+++ gerenciador/src/br/com/alura/gerenciador/servlet/NovaEmpresaServlet.java
@@ -13,7 +13,7 @@ import javax.servlet.http.HttpServletResponse;
  * Servlet implementation class NovaEmpresaSerlet
  */
 @WebServlet("/novaEmpresa")
-public class NovaEmpresaSerlet extends HttpServlet {
+public class NovaEmpresaServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
