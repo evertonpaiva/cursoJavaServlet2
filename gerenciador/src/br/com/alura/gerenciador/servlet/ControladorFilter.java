64faeebc9d1b979bdaa189443ebdb378f8043d76
Everton Paiva <evertonpaiva@gmail.com>
2019-07-17 15:24:29 -0300

    Alterando ordem de execução dos filtros

--- gerenciador/src/br/com/alura/gerenciador/servlet/ControladorFilter.java
+++ gerenciador/src/br/com/alura/gerenciador/servlet/ControladorFilter.java
@@ -1,26 +1,31 @@
 package br.com.alura.gerenciador.servlet;
 
 import java.io.IOException;
-
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import br.com.alura.gerenciador.acao.Acao;
 
-//@WebServlet("/entrada")
-public class UnicaEntradaServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
+//@WebFilter("/entrada")
+public class ControladorFilter implements Filter {
 
-	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain chain) throws IOException, ServletException {
 		
-		String paramAcao = request.getParameter("acao");
+		System.out.println("ControladorFilter");
 		
-		String nomeDaClasse = "br.com.alura.gerenciador.acao." + paramAcao;
+		HttpServletRequest request = (HttpServletRequest) servletRequest;
+		HttpServletResponse response = (HttpServletResponse) servletResponse;
 		
+		String paramAcao = request.getParameter("acao");		
+		String nomeDaClasse = "br.com.alura.gerenciador.acao." + paramAcao;		
 		String nome;
 		try {
 			Class classe = Class.forName(nomeDaClasse);//carrega a classe com o nome 
