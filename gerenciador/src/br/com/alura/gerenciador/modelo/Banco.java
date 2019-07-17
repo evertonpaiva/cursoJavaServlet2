820051f7da42871329722e5ca876d1f375e82fef
Everton Paiva <evertonpaiva@gmail.com>
2019-07-17 12:55:44 -0300

    Preparando um formulário de Login

--- gerenciador/src/br/com/alura/gerenciador/modelo/Banco.java
+++ gerenciador/src/br/com/alura/gerenciador/modelo/Banco.java
@@ -7,17 +7,31 @@ import java.util.List;
 public class Banco {
 
 	private static List<Empresa> lista = new ArrayList<>();
+	private static List<Usuario> listaUsuarios = new ArrayList<>();
 	private static Integer chaveSequencial = 1;
 	
 	static {
 		Empresa empresa1 = new Empresa();
 		empresa1.setNome("Alura");
 		empresa1.setId(Banco.chaveSequencial++);
+		
 		Empresa empresa2 = new Empresa();
 		empresa2.setNome("Caelum");
 		empresa2.setId(Banco.chaveSequencial++);
+		
 		Banco.lista.add(empresa1);
 		Banco.lista.add(empresa2);
+		
+		Usuario u1 = new Usuario();
+		u1.setLogin("everton");
+		u1.setSenha("123456");
+		
+		Usuario u2 = new Usuario();
+		u2.setLogin("nico");
+		u2.setSenha("123456");
+		
+		Banco.listaUsuarios.add(u1);
+		Banco.listaUsuarios.add(u2);
 	}
 	
 	public void adiciona(Empresa empresa) {
