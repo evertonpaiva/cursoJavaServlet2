bc25903d4b22ef5dd9598030489363df1d460e8d
Everton Paiva <evertonpaiva@gmail.com>
2019-07-17 12:00:18 -0300

    Escondendo os arquivos JSP

--- gerenciador/src/br/com/alura/gerenciador/acao/ListaEmpresas.java
+++ gerenciador/src/br/com/alura/gerenciador/acao/ListaEmpresas.java
@@ -13,13 +13,12 @@ import br.com.alura.gerenciador.modelo.Empresa;
 
 public class ListaEmpresas {
 
-	public void executa(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public String executa(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		System.out.println("acao listando empresas");
 		Banco banco = new Banco();
 		List<Empresa> lista = banco.getEmpresas();		
 		request.setAttribute("empresas", lista);
 		
-		RequestDispatcher rd = request.getRequestDispatcher("/listaEmpresas.jsp");
-		rd.forward(request, response);
+		return "forward:listaEmpresas.jsp";
 	}
 }
